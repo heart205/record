038147ca660c9db070cead1e5f9729ddbd4f6b59
heart <7362469@qq.com>
2022-07-14 23:28:34 +0800

    chore: update taskLoop

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
-// var http = require('http');
+var http = require('http');
+var { readEnvFile } = require('../utils/readEnvFile.cjs');
 
-// const req = http.request('http://8.210.91.28:4000/', (res) => {
-//   let data = '';
+const envObject = readEnvFile(
+  process.cwd() + `/.${isProduction ? 'production' : 'development'}.env`,
+);
+const req = http.request(`${envObject.baseUrl}`, (res) => {
+  let data = '';
 
-//   res.on('data', function (chunk) {
-//     data += chunk;
-//   });
+  res.on('data', function (chunk) {
+    data += chunk;
+  });
 
-//   res.on('end', function () {
-//     console.log('http.request end data:', data.toString());
-//   });
-// });
+  res.on('end', function () {
+    console.log('http.request end data:', data.toString());
+  });
+});
 
-// req.on('error', (e) => {
-//   console.error(`problem with request: ${e.message}`);
-// });
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
 
-// req.end();
+req.end();
 
-// // 请求今天是周几
-
-// const date = new Date();
-// console.log(date.getDay());
+// 请求今天是周几
+const date = new Date();
+console.log(date.getDay());
